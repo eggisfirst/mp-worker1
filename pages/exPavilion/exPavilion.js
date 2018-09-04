d204cd37d1d6eb8df06ba9278733dff16bc48bc1
unknown <you@example.com>
2018-09-04 11:47:18 +0800

    增加下拉菜单

---
+++ pages/exPavilion/exPavilion.js
@@ -0,0 +1,27 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    open: false,
+    isSelected:false,
+    color:'#e6e6e6'
+
+  },
+  showitem: function () {
+    this.setData({
+      open: !this.data.open
+    })
+  },
+  selected:function(){
+    this.setData({
+      
+    })
+  }
+
+
+
+
+
+})
