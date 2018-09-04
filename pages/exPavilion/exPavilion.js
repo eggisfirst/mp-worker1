2f4a0a3e5bec948da1ae018ed095fb030744a002
unknown <you@example.com>
2018-09-04 18:06:05 +0800

    1

--- pages/exPavilion/exPavilion.js
+++ pages/exPavilion/exPavilion.js
@@ -1,27 +1,68 @@
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-    open: false,
-    isSelected:false,
-    color:'#e6e6e6'
-
+    select: true,
+    time: '全部',
+    num:'',
+    array1:[{
+      message1:'您认为本公司枕头的舒适度如何？'
+    },{
+      message1:"您认为本公司床垫的舒适度如何？"
+    }
+    ],
+    array2:[{
+      message2:'非常满意'
+    },{
+      message2:'满意'
+    },{
+      message2:'太高或太矮'
+    },{
+      message2:'太软或太硬'
+    }
+    ],
+    array3:[{
+      message3:'80%'
+    },{
+      message3:'8%'
+    },{
+      message3:'2%'
+    },{
+      message3:'10%'
+    }]
   },
-  showitem: function () {
+  //点击选择类型，出现/隐藏
+  showTime: function () {
+    var select = this.data.select;
+    if (select == true) {
+      this.setData({
+        select: false
+      })
+    } else {
+      this.setData({
+        select: true
+      })
+    }
+  },
+  //点击切换样式，顶部time值改变
+  mySelect: function (e) {
     this.setData({
-      open: !this.data.open
+      time: e.target.dataset.me,
+      num:e.target.dataset.num,
+      select: true,
     })
   },
-  selected:function(){
-    this.setData({
-      
-    })
+  onLoad: function (options) {
+    // 页面初始化 options为页面跳转所带来的参数
+  },
+  onReady: function () {
+    // 页面渲染完成
+  },
+  onShow: function () {
+    // 页面显示
+  },
+  onHide: function () {
+    // 页面隐藏
+  },
+  onUnload: function () {
+    // 页面关闭
   }
-
-
-
-
-
 })
